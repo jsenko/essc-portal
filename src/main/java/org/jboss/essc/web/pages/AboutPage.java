16afbec21d14c1028fd824371cdff3d00c99a814
Ondrej Zizka <zizka@seznam.cz>
2012-04-11 04:38:04 +0200

    Added tests, ARQ deps, and fixed pages markup.

--- src/main/java/org/jboss/essc/web/pages/AboutPage.java
+++ src/main/java/org/jboss/essc/web/pages/AboutPage.java
@@ -1,27 +1,14 @@
 package org.jboss.essc.web.pages;
 
-import javax.inject.Inject;
-import org.apache.wicket.markup.html.IHeaderResponse;
-
-import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.request.resource.CssResourceReference;
-import org.jboss.essc.web.dao.ContactDao;
-import org.jboss.essc.web.model.Contact;
-
 /**
  * Dynamic behavior for the ListContact page
  * 
  * @author Ondrej Zizka
  */
 @SuppressWarnings("serial")
-public class AboutPage extends WebPage {
+public class AboutPage extends BaseLayoutPage {
 
 
-    // Set up the dynamic behavior for the page, widgets bound by id
     public AboutPage() {
     }
     
