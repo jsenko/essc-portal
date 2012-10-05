4e61109d2fa33f10a6f1d5c925956c9625652d72
Ondrej Zizka <ozizka@redhat.com>
2012-10-05 13:05:38 +0200

    Add Dismiss link to AboutSmallBox.

--- src/main/java/org/jboss/essc/web/_cp/pageBoxes/AboutSmallBox.java
+++ src/main/java/org/jboss/essc/web/_cp/pageBoxes/AboutSmallBox.java
@@ -1,6 +1,11 @@
 package org.jboss.essc.web._cp.pageBoxes;
 
+import javax.servlet.http.Cookie;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
 
 
 /**
@@ -10,9 +15,28 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public class AboutSmallBox extends Panel {
 
+    private final static String COOKIE_HIDE_ABOUT_BOX = "essc.hideAboutBox";
+
     
     public AboutSmallBox( String id ) {
         super(id);
+        setOutputMarkupId( true );
+
+        Cookie cookieHideAbout = ((WebRequest)getRequestCycle().getRequest()).getCookie(COOKIE_HIDE_ABOUT_BOX);
+        if( cookieHideAbout != null )
+            this.setVisibilityAllowed( false );
+        
+
+        // Hides the box and stores that info to cookies.
+        add( new AjaxLink("dismiss"){
+
+            @Override public void onClick( AjaxRequestTarget target ) {
+                target.add( AboutSmallBox.this );
+                AboutSmallBox.this.setVisible( false );
+                ((WebResponse)getRequestCycle().getResponse()).addCookie(new Cookie(COOKIE_HIDE_ABOUT_BOX, "true"));
+            }
+            
+        });
         
     }// const
 
